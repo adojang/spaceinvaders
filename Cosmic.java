9c6bc6cb96a93bcc476f51505b4df9ff40c427b8
adojang <masterkittypaws@gmail.com>
2019-03-06 15:49:01 +0200

    First version of Cosmic. Added Keyboard Support

---
+++ Cosmic.java
@@ -0,0 +1,33 @@
+/* 
+ * Coded by Adriaan van Wijk || 2019
+ * <Insert Professional Quip or Joke Here>
+ */
+
+public class Cosmic
+{
+  public static void main(String[] args)
+  {
+    //Keyboard Controls Section
+    
+    
+    //Initializations
+   
+    
+    
+    
+    //Main Menu Start
+      //draw main menu function
+    
+    while(!StdDraw.isKeyPressed(32))
+    {
+      
+    }
+    
+    //START
+    while(!StdDraw.isKeyPressed(81))
+    {
+     //Display the First frame of the game. 
+    }
+    
+  } 
+}
