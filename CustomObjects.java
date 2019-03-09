469bc3dd54fbdc16933cb16bf2c05579ff8a9b5c
adojang <masterkittypaws@gmail.com>
2019-03-09 14:05:38 +0200

    Custom Objects Library. Currently contains ship

---
+++ CustomObjects.java
@@ -0,0 +1,16 @@
+public class CustomObjects
+{
+ 
+  
+  public void ship(double x, double y)
+  {
+ 
+    
+    
+    //This ship is sourced from http://millionthvector.blogspot.com/2015/11/
+    //According to the licence we just need to give credit to the creator.
+   StdDraw.picture( x,  y,  "ship.png", 0);
+   
+  
+  }
+}
