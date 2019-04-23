b39864ac97490fd15ad8a460bc58adcef2614e58
adojang <masterkittypaws@gmail.com>
2019-04-23 04:59:00 +0200

    Comments Everywhere and set Shark HP = 5

--- PlayerEntity.java
+++ PlayerEntity.java
@@ -36,14 +36,7 @@ public class PlayerEntity extends Entity
         if(UserInput.checkKeyPressed("LEFT") == true && xPos > 0) xPos = xPos - velocity;
         if(UserInput.checkKeyPressed("UP") == true && yPos < 200) yPos = yPos + velocity;
         if(UserInput.checkKeyPressed("DOWN") == true && yPos > 0) yPos = yPos - velocity;
-        
-        
-        /*if(UserInput.checkUserInput() == "RIGHT" && xPos < 1024) xPos = xPos + velocity;
-        if(UserInput.checkUserInput() == "LEFT" && xPos > 0) xPos = xPos - velocity;
-        if(UserInput.checkUserInput() == "UP" && yPos < 700) yPos = yPos + velocity;
-        if(UserInput.checkUserInput() == "DOWN" && yPos > 0) yPos = yPos - velocity;*/
-        
-        //rotation through mouse
+
         double radians = 0;
         if(StdDraw.mouseX() > xPos && StdDraw.mouseY() > yPos)
         {
@@ -59,27 +52,7 @@ public class PlayerEntity extends Entity
             rotation = 90 + (int) Math.toDegrees(radians);
             
         }
-        /*if(StdDraw.mouseX() < xPos && StdDraw.mouseY() < yPos)
-        {
-            
-            radians = Math.atan((StdDraw.mouseY()-yPos)/(StdDraw.mouseX()-xPos));
-            rotation = 90 + (int) Math.toDegrees(radians);
-            
-        }
-        if(StdDraw.mouseX() > xPos && StdDraw.mouseY() < yPos)
-        {
-            
-            radians = Math.atan((StdDraw.mouseY()-yPos)/(StdDraw.mouseX()-xPos));
-            rotation = (int) Math.toDegrees(radians) - 90;
-            
-        }*/
-        //rotation through mouse
-     
-        //rotation through keys
-        //if(UserInput.checkUserInput() == "ROTATELEFT" && rotation < 85) rotation = rotation + 5;
-        //if(UserInput.checkUserInput() == "ROTATERIGHT" && rotation > -85) rotation = rotation - 5;
-        //rotation through keys
-        
+ 
     }//update
     
     public static int currentHP(int modifyHP)
